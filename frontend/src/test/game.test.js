e824c24957f0479cab0c246387cb8606711b0710
EC2 Default User <ec2-user@ip-172-31-41-173.us-east-2.compute.internal>
2021-01-29 16:44:28 +0000

    Fixed imports and added AlgebreicNotation() test

--- frontend/src/test/game.test.js
+++ frontend/src/test/game.test.js
@@ -1,10 +1,21 @@
 import boardInit from '../helpers/BoardInit.js';
+import {
+  getKingPosition,
+  isCheck,
+  isMate,
+  pathIsClear,
+  algebreicNotation
+} from '../components/Game.js';
 
-squares = boardInit();
-const getKingPosition = require('./getKingPosition');
+const squares = boardInit();
+
 
 
 
 test('player 1 king at position 4', () => {
   expect(getKingPosition(squares, 1)).toBe(4);
+});
+
+test('Square 4 is e1', () => {
+  expect(algebreicNotation(4)).toBe('e1');
 });
